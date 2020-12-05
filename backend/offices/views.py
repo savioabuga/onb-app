863fe6d8a9b2246ed14465aac32815ac26afe0e8
Savio Abuga <40393235+savioabuga@users.noreply.github.com>
2020-12-05 06:24:47 +0300

    Expose region to the api #5

--- backend/offices/views.py
+++ backend/offices/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Region
 
-# Create your views here.
+
+class RegionView(APIView):
+    
+    def get(self, request, format=None):
+        features = []
+        for region in Region.objects.all():
+            features.append({"geometry": region.polygon.json, "properties": {"name": region.name }})
+
+        return Response(
+            {
+              "type": "FeatureCollection",
+              "name": "OBN Regions",
+              "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+              "features": features
+            }
+        )
